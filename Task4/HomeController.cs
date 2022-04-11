946bd39e9c2a05f6d47b9951e45e453cd54833cd
Nikita Slavin <nikitapowerslavin@gmail.com>
2022-04-11 23:30:05 +0300

    first commit

---
+++ Task4/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Task4
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Dictionary<string, object> data = new Dictionary<string, object>();
+            data.Add("Ключ", "Значение");
+
+            return View(data);
+        }
+    }
+}
